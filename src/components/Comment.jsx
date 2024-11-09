8cdb3215e31a72c59cfaaf2ad9ff83361cfc2a49
arthur <contato.arthurferreiraa@gmail.com>
2024-11-09 09:35:22 -0300

    Adding likes and comment conditions

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,8 +1,23 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './comment.module.css'
 import { Avatar } from './Avatar'
+import { useState } from 'react'
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        console.log('Deletado')
+
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        })
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/Arthurferre1ra.png"/>
@@ -17,7 +32,7 @@ export function Comment({ content }) {
                             <time title='25 de outubro as 09 e 10' dateTime='2024-10-25 09:10:20'>Cerca de 1h atrás</time>
                         </div>
 
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24} />
                         </button>
                     </header>
@@ -26,9 +41,9 @@ export function Comment({ content }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>20</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
 
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,8 +32,21 @@ function handleCreateNewComment() {
     setNewCommentText('')
 }
 
+function handleNewCommentInvalid() {
+    event.target.setCustomValidity('Esse campo é obrigatório!');
+}
+
 function handleNewCommentChange() {
-    setNewCommentText(event.target.value)
+    event.target.setCustomValidity('');
+    setNewCommentText(event.target.value);
+}
+
+function deleteComment(commentToDelete) {
+    const commentsWithoutDeleteOne = comments.filter(comment => {
+        return comment != commentToDelete;
+    })
+
+    setComments(commentsWithoutDeleteOne);
 }
 
     return (
@@ -57,9 +70,9 @@ function handleNewCommentChange() {
             <div className={styles.content}>
                     {content.map(line => {
                         if (line.type == 'paragraph') {
-                            return <p>{line.content}</p>
+                            return <p key={line.content}>{line.content}</p>
                         } else if (line.type == 'link') {
-                            return <p><a href="#">{line.content}</a></p>
+                            return <p key={line.content}><a href="#">{line.content}</a></p>
                         }
                     })}
             </div>
@@ -72,16 +85,18 @@ function handleNewCommentChange() {
                 value={newCommentText}
                 placeholder='Deixe um comentário'
                 onChange={handleNewCommentChange}
+                onInvalid={handleNewCommentInvalid}
+                required
                 />
 
                 <footer>
-                    <button type='submit'>Publicar</button>
+                    <button type='submit' >Publicar</button>
                 </footer>
             </form>
 
             <div className={styles.CommentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment}/>
+                    return <Comment key={comment} content={comment} onDeleteComment={deleteComment}/>
                 })}
                 
             </div>
